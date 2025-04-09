f8cb6b6e6d4765e841ca6505d9577d99fe504500
thanhhai-beep <dohai30112002@gmail.com>
2025-04-09 08:14:32 +0700

    hh

--- admin-panel/tailwind.config.js
+++ admin-panel/tailwind.config.js
@@ -19,9 +19,12 @@ module.exports = {
           700: '#0369a1',
           800: '#075985',
           900: '#0c4a6e',
+          950: '#082f49',
         },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 } 
